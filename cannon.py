bde2e49b7d65100e2791fe66e7c44098f1e7b45e
Иван Лунев <ivanlunev@MacBook-Pro-Ivan.local>
2020-11-01 11:48:43 +0300

    Initial code refactoring

--- cannon.py
+++ cannon.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pygame as pg
-from random import randint, gauss
+from random import randint  # , gauss
 
 pg.init()
 pg.font.init()
@@ -21,26 +21,26 @@ class GameObject:
 
 
 class Shell(GameObject):
-    '''
+    """
     The ball class. Creates a ball, controls it's movement and implement it's rendering.
-    '''
+    """
 
     def __init__(self, coord, vel, rad=20, color=None):
-        '''
+        """
         Constructor method. Initializes ball's parameters and initial values.
-        '''
+        """
         self.coord = coord
         self.vel = vel
-        if color == None:
+        if color is None:
             color = rand_color()
         self.color = color
         self.rad = rad
         self.is_alive = True
 
     def check_corners(self, refl_ort=0.8, refl_par=0.9):
-        '''
-        Reflects ball's velocity when ball bumps into the screen corners. Implemetns inelastic rebounce.
-        '''
+        """
+        Reflects ball's velocity when ball bumps into the screen corners. Implements inelastic rebound.
+        """
         for i in range(2):
             if self.coord[i] < self.rad:
                 self.coord[i] = self.rad
@@ -52,10 +52,10 @@ class Shell(GameObject):
                 self.vel[1 - i] = int(self.vel[1 - i] * refl_par)
 
     def move(self, time=1, grav=0):
-        '''
+        """
         Moves the ball according to it's velocity and time step.
         Changes the ball's velocity due to gravitational force.
-        '''
+        """
         self.vel[1] += grav
         for i in range(2):
             self.coord[i] += time * self.vel[i]
@@ -64,21 +64,23 @@ class Shell(GameObject):
             self.is_alive = False
 
     def draw(self, screen):
-        '''
+        """
         Draws the ball on appropriate surface.
-        '''
+        """
         pg.draw.circle(screen, self.color, self.coord, self.rad)
 
 
 class Cannon(GameObject):
-    '''
-    Cannon class. Manages it's renderring, movement and striking.
-    '''
+    """
+    Cannon class. Manages it's rendering, movement and striking.
+    """
 
-    def __init__(self, coord=[30, SCREEN_SIZE[1] // 2], angle=0, max_pow=50, min_pow=10, color=RED):
-        '''
+    def __init__(self, coord=None, angle=0, max_pow=50, min_pow=10, color=RED):
+        """
         Constructor method. Sets coordinate, direction, minimum and maximum power and color of the gun.
-        '''
+        """
+        if coord is None:
+            coord = [30, SCREEN_SIZE[1] // 2]
         self.coord = coord
         self.angle = angle
         self.max_pow = max_pow
@@ -88,22 +90,22 @@ class Cannon(GameObject):
         self.pow = min_pow
 
     def activate(self):
-        '''
+        """
         Activates gun's charge.
-        '''
+        """
         self.active = True
 
     def gain(self, inc=2):
-        '''
+        """
         Increases current gun charge power.
-        '''
+        """
         if self.active and self.pow < self.max_pow:
             self.pow += inc
 
     def strike(self):
-        '''
+        """
         Creates ball, according to gun's direction and current charge power.
-        '''
+        """
         vel = self.pow
         angle = self.angle
         ball = Shell(list(self.coord), [int(vel * np.cos(angle)), int(vel * np.sin(angle))])
@@ -112,22 +114,22 @@ class Cannon(GameObject):
         return ball
 
     def set_angle(self, target_pos):
-        '''
+        """
         Sets gun's direction to target position.
-        '''
+        """
         self.angle = np.arctan2(target_pos[1] - self.coord[1], target_pos[0] - self.coord[0])
 
     def move(self, inc):
-        '''
+        """
         Changes vertical position of the gun.
-        '''
+        """
         if (self.coord[1] > 30 or inc > 0) and (self.coord[1] < SCREEN_SIZE[1] - 30 or inc < 0):
             self.coord[1] += inc
 
     def draw(self, screen):
-        '''
+        """
         Draws the gun on the screen.
-        '''
+        """
         gun_shape = []
         vec_1 = np.array([int(5 * np.cos(self.angle - np.pi / 2)), int(5 * np.sin(self.angle - np.pi / 2))])
         vec_2 = np.array([int(self.pow * np.cos(self.angle)), int(self.pow * np.sin(self.angle))])
@@ -140,35 +142,35 @@ class Cannon(GameObject):
 
 
 class Target(GameObject):
-    '''
+    """
     Target class. Creates target, manages it's rendering and collision with a ball event.
-    '''
+    """
 
     def __init__(self, coord=None, color=None, rad=30):
-        '''
+        """
         Constructor method. Sets coordinate, color and radius of the target.
-        '''
-        if coord == None:
+        """
+        if coord is None:
             coord = [randint(rad, SCREEN_SIZE[0] - rad), randint(rad, SCREEN_SIZE[1] - rad)]
         self.coord = coord
         self.rad = rad
 
-        if color == None:
+        if color is None:
             color = rand_color()
         self.color = color
 
     def check_collision(self, ball):
-        '''
+        """
         Checks whether the ball bumps into target.
-        '''
+        """
         dist = sum([(self.coord[i] - ball.coord[i]) ** 2 for i in range(2)]) ** 0.5
         min_dist = self.rad + ball.rad
         return dist <= min_dist
 
     def draw(self, screen):
-        '''
+        """
         Draws the target on the screen
-        '''
+        """
         pg.draw.circle(screen, self.color, self.coord, self.rad)
 
     def move(self):
@@ -191,9 +193,9 @@ class MovingTarget(Target):
 
 
 class ScoreTable:
-    '''
+    """
     Score table class.
-    '''
+    """
 
     def __init__(self, t_destr=0, b_used=0):
         self.t_destr = t_destr
@@ -201,24 +203,25 @@ class ScoreTable:
         self.font = pg.font.SysFont("dejavusansmono", 25)
 
     def score(self):
-        '''
+        """
         Score calculation method.
-        '''
+        """
         return self.t_destr - self.b_used
 
     def draw(self, screen):
-        score_surf = []
-        score_surf.append(self.font.render("Destroyed: {}".format(self.t_destr), True, WHITE))
-        score_surf.append(self.font.render("Balls used: {}".format(self.b_used), True, WHITE))
-        score_surf.append(self.font.render("Total: {}".format(self.score()), True, RED))
+        score_surf = [
+            self.font.render("Destroyed: {}".format(self.t_destr), True, WHITE),
+            self.font.render("Balls used: {}".format(self.b_used), True, WHITE),
+            self.font.render("Total: {}".format(self.score()), True, RED)
+        ]
         for i in range(3):
             screen.blit(score_surf[i], [10, 10 + 30 * i])
 
 
 class Manager:
-    '''
+    """
     Class that manages events' handling, ball's motion and collision, target creation, etc.
-    '''
+    """
 
     def __init__(self, n_targets=1):
         self.balls = []
@@ -229,9 +232,9 @@ class Manager:
         self.new_mission()
 
     def new_mission(self):
-        '''
+        """
         Adds new targets.
-        '''
+        """
         for i in range(self.n_targets):
             self.targets.append(Target(rad=randint(max(1, 30 - 2 * max(0, self.score_t.score())),
                                                    30 - max(0, self.score_t.score()))))
@@ -240,9 +243,9 @@ class Manager:
                                                          30 - max(0, self.score_t.score()))))
 
     def process(self, events, screen):
-        '''
+        """
         Runs all necessary method for each iteration. Adds new targets, if previous are destroyed.
-        '''
+        """
         done = self.handle_events(events)
 
         if pg.mouse.get_focused():
@@ -259,9 +262,9 @@ class Manager:
         return done
 
     def handle_events(self, events):
-        '''
+        """
         Handles events from keyboard, mouse, etc.
-        '''
+        """
         done = False
         for event in events:
             if event.type == pg.QUIT:
@@ -281,9 +284,9 @@ class Manager:
         return done
 
     def draw(self, screen):
-        '''
+        """
         Runs balls', gun's, targets' and score table's drawing method.
-        '''
+        """
         for ball in self.balls:
             ball.draw(screen)
         for target in self.targets:
@@ -292,9 +295,9 @@ class Manager:
         self.score_t.draw(screen)
 
     def move(self):
-        '''
+        """
         Runs balls' and gun's movement method, removes dead balls.
-        '''
+        """
         dead_balls = []
         for i, ball in enumerate(self.balls):
             ball.move(grav=2)
@@ -307,9 +310,9 @@ class Manager:
         self.gun.gain()
 
     def collide(self):
-        '''
+        """
         Checks whether balls bump into targets, sets balls' alive trigger.
-        '''
+        """
         collisions = []
         targets_c = []
         for i, ball in enumerate(self.balls):
@@ -323,19 +326,19 @@ class Manager:
             self.targets.pop(j)
 
 
-screen = pg.display.set_mode(SCREEN_SIZE)
-pg.display.set_caption("The gun of Khiryanov")
+main_screen = pg.display.set_mode(SCREEN_SIZE)
+pg.display.set_caption("Gun Game 2")
 
-done = False
+game_done = False
 clock = pg.time.Clock()
 
 mgr = Manager(n_targets=3)
 
-while not done:
+while not game_done:
     clock.tick(15)
-    screen.fill(BLACK)
+    main_screen.fill(BLACK)
 
-    done = mgr.process(pg.event.get(), screen)
+    game_done = mgr.process(pg.event.get(), main_screen)
 
     pg.display.flip()
 
